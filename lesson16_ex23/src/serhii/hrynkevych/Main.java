e66d69531b1980af01950c03087704825123bc78
Serhii Hrynkevych <s.hrynkevych20@gmail.com>
2021-11-21 22:51:28 +0200

    Edit homework number 23, lesson 16

--- lesson16_ex23/src/serhii/hrynkevych/Main.java
+++ lesson16_ex23/src/serhii/hrynkevych/Main.java
@@ -9,20 +9,30 @@ public class Main {
 
     public static void main(String[] args) {
         List<String> listArray = addedElementListArray(10);
+        System.out.println(listArray);
         List<String> listLink = addedElementLinkedList(10);
-        System.out.println("Задания №№1, 2:");
-        System.out.println(listArray);
-        System.out.println(listLink);
-        System.out.println();
-        System.out.println("Задание №№3:");
-        System.out.println(listArray);
         System.out.println(listLink);
         ex3(listArray, listLink);
-        System.out.println();
-        System.out.println("Задание №№4:");
-        System.out.println(listArray);
-        System.out.println(listLink);
         ex4(listArray, listLink);
+        System.out.println(listLink);
+
+//        System.out.println("Задания №№1, 2:");
+//        System.out.println(listArray);
+//        System.out.println(listLink);
+//        System.out.println();
+//        System.out.println("Задание №№3:");
+//        System.out.println(listArray);
+//        System.out.println(listLink);
+//        ex3(listArray, listLink);
+//        System.out.println();
+//        System.out.println("Задание №№4:");
+//        System.out.println(listArray);
+//        System.out.println(listLink);
+//        ex4(listArray, listLink);
+//        System.out.println();
+//        System.out.println("Задание №№5:");
+//        System.out.println(listArray);
+//        System.out.println(listLink);
     }
 
     public static List<String> addedElementListArray(int elements) {
@@ -42,27 +52,25 @@ public class Main {
     }
 
     public static void ex3 (List<String> listArray, List<String> listLink) {
-        List<String> listLinkEx3 = new LinkedList<>(listLink);
         ListIterator<String> listIteratorEx3 = listArray.listIterator(listArray.size());
         while (listIteratorEx3.hasPrevious()) {
-            int indexAdd = Math.abs(((listIteratorEx3.previousIndex() + 1) - listLinkEx3.size()));
-            listLinkEx3.add(indexAdd, listIteratorEx3.previous());
+            int indexAdd = Math.abs(((listIteratorEx3.previousIndex() + 1) - listLink.size()));
+            listLink.add(indexAdd, listIteratorEx3.previous());
         }
-        System.out.println(listLinkEx3);
+        //System.out.println(listLink);
     }
 
     public static void ex4 (List<String> listArray, List<String> listLink) {
-        List<String> listLinkEx4 = new LinkedList<>(listLink);
         ListIterator<String> listIteratorEx4 = listArray.listIterator(listArray.size());
         int nextIndexAdd = 0;
         while (listIteratorEx4.hasPrevious()) {
-            if (nextIndexAdd <= listLinkEx4.size()) {
-                listLinkEx4.add(nextIndexAdd, listIteratorEx4.previous());
+            if (nextIndexAdd <= listLink.size()) {
+                listLink.add(nextIndexAdd, listIteratorEx4.previous());
                 nextIndexAdd +=3;
             } else {
-                listLinkEx4.add(listIteratorEx4.previous());
+                listLink.add(listIteratorEx4.previous());
             }
         }
-        System.out.println(listLinkEx4);
+        //System.out.println(listLink);
     }
 }
